d2b2ff1f1ff36e9b332fc2349c1f7a943f3fe5e1
Mateus Rovari <mateus@liveonsolutions.com>
2018-07-19 10:39:45 -0300

    Profile Toolbar

--- app/src/main/java/com/example/mateusrovari/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/mateusrovari/instagramclone/Profile/ProfileActivity.java
@@ -7,10 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import com.example.mateusrovari.instagramclone.R;
 import com.example.mateusrovari.instagramclone.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.logging.LoggingPermission;
 
 public class ProfileActivity extends AppCompatActivity{
 
@@ -22,10 +25,29 @@ public class ProfileActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
 
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+        setupToolbar();
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = findViewById(R.id.profileToolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigating to Profile Preference");
+                }
+                return false;
+            }
+        });
     }
 
     //    BottomNavigationView setup
@@ -37,5 +59,11 @@ public class ProfileActivity extends AppCompatActivity{
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
