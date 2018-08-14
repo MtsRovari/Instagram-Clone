730771ea42ff229b97cd2f52304f9a390afe9571
Mateus Rovari <mateus@liveonsolutions.com>
2018-08-14 14:56:37 -0300

    setting editProfile Fragment widgets

--- app/src/main/java/com/example/mateusrovari/instagramclone/Profile/EditProfileFragment.java
+++ app/src/main/java/com/example/mateusrovari/instagramclone/Profile/EditProfileFragment.java
@@ -2,32 +2,71 @@ package com.example.mateusrovari.instagramclone.Profile;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mateusrovari.instagramclone.R;
+import com.example.mateusrovari.instagramclone.Utils.FirebaseMethods;
 import com.example.mateusrovari.instagramclone.Utils.UniversalImageLoader;
+import com.example.mateusrovari.instagramclone.models.User;
+import com.example.mateusrovari.instagramclone.models.UserAccountSettings;
+import com.example.mateusrovari.instagramclone.models.UserSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EditProfileFragment extends Fragment {
 
-    private ImageView mProfilePhoto;
+
     private ImageView backArrow;
 
+    //    firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListner;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
+
     private static final String TAG = "EditProfileFragment";
+
+//    EditProfileFragment widgets
+    private EditText mDisplayName, mUsername, mWebsite, mDescription, mEmail, mPhoneNumber;
+    private TextView mChangeProfilePhoto;
+    private CircleImageView mProfilePhoto;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_editprofile, container, false);
         mProfilePhoto = view.findViewById(R.id.profile_photo);
+        mDisplayName = view.findViewById(R.id.editProfile_display_name);
+        mUsername = view.findViewById(R.id.editProfile_username);
+        mWebsite = view.findViewById(R.id.editProfile_website);
+        mDescription = view.findViewById(R.id.editProfile_description);
+        mEmail = view.findViewById(R.id.editProfile_email);
+        mPhoneNumber = view.findViewById(R.id.editProfile_phoneNumber);
+        mChangeProfilePhoto = view.findViewById(R.id.changeProfilePhoto);
 
-        setProfileImage();
+        mFirebaseMethods = new FirebaseMethods(getActivity());
+
+//        setProfileImage();
+        setupFirebaseAuth();
 
 //      back arrow for navigating back to "ProfileActivity"
         backArrow = view.findViewById(R.id.backToProfile);
@@ -40,9 +79,84 @@ public class EditProfileFragment extends Fragment {
         return view;
     }
 
-    private  void setProfileImage(){
-        Log.d(TAG, "setProfileImage: setting profile image");
-        String imgURL = "media.licdn.com/dms/image/C4D03AQES5n0xQz-xtQ/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=WhJKFmuQoQ3MMzSp9SzQT_uVeTfrAsYhKBuhBqQa8hA";
-        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
+//    private  void setProfileImage(){
+//        Log.d(TAG, "setProfileImage: setting profile image");
+//        String imgURL = "media.licdn.com/dms/image/C4D03AQES5n0xQz-xtQ/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=WhJKFmuQoQ3MMzSp9SzQT_uVeTfrAsYhKBuhBqQa8hA";
+//        UniversalImageLoader.setImage(imgURL, mProfilePhoto, null, "https://");
+//    }
+
+    private void setProfileWidgets(UserSettings userSettings) {
+
+        User user = userSettings.getUser();
+        UserAccountSettings settings = userSettings.getSettings();
+
+        UniversalImageLoader.setImage(settings.getProfile_photo(), mProfilePhoto, null, "");
+
+        mDisplayName.setText(settings.getDisplay_name());
+        mUsername.setText(settings.getUsername());
+        mWebsite.setText(settings.getWebsite());
+        mDescription.setText(settings.getDescription());
+        mEmail.setText(userSettings.getUser().getEmail());
+        mPhoneNumber.setText(String.valueOf(userSettings.getUser().getPhone_number()));
+    }
+
+    /**
+     ---------------------------------- Firebase -------------------------------------
+     */
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebase: starting");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //user is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in" + user.getUid());
+                } else {
+                    //user is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //retrieve user information from the database
+                setProfileWidgets(mFirebaseMethods.getUserSettings(dataSnapshot));
+
+                //retrieve images for the user in question
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListner != null) {
+            mAuth.removeAuthStateListener(mAuthListner);
+        }
     }
 }
