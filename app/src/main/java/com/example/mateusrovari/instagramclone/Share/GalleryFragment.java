b35cf5c90da89db5a7620864e0aff1eb75c65417
Mateus Rovari <mateus@liveonsolutions.com>
2018-08-16 13:55:51 -0300

    gallery fragment layout

--- app/src/main/java/com/example/mateusrovari/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/com/example/mateusrovari/instagramclone/Share/GalleryFragment.java
@@ -3,19 +3,55 @@ package com.example.mateusrovari.instagramclone.Share;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.mateusrovari.instagramclone.R;
 
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
 
+    //widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner directorySpinner;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        Log.d(TAG, "onCreateView: started");
+
+        galleryImage = view.findViewById(R.id.galleryImageView);
+        gridView = view.findViewById(R.id.gridImageView);
+        mProgressBar = view.findViewById(R.id.galleryProgressBar);
+        directorySpinner = view.findViewById(R.id.gallerySpinnerDirectory);
+
+        mProgressBar.setVisibility(View.GONE);
+        ImageView shareClose = view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        TextView nextScreen = view.findViewById(R.id.galleryNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to the final share screen");
+
+            }
+        });
 
         return view;
     }
