f5657a83b62c59a249c74e58f8e00428d474828f
Mateus Rovari <mateus@liveonsolutions.com>
2018-08-17 18:30:07 -0300

    share activity gridview with phone directories

--- app/src/main/java/com/example/mateusrovari/instagramclone/Share/GalleryFragment.java
+++ app/src/main/java/com/example/mateusrovari/instagramclone/Share/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.mateusrovari.instagramclone.Share;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -14,15 +17,30 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.mateusrovari.instagramclone.R;
+import com.example.mateusrovari.instagramclone.Utils.FilePaths;
+import com.example.mateusrovari.instagramclone.Utils.FileSearch;
+import com.example.mateusrovari.instagramclone.Utils.GridImageAdapter;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
     private static final String TAG = "GalleryFragment";
+
+    //constants
+    private static final int NUM_GRID_COLUMNS = 3;
 
     //widgets
     private GridView gridView;
     private ImageView galleryImage;
     private ProgressBar mProgressBar;
     private Spinner directorySpinner;
+
+    //vars
+    private ArrayList<String> directories;
+    private String mAppend = "file:/";
 
     @Nullable
     @Override
@@ -31,9 +49,10 @@ public class GalleryFragment extends Fragment {
         Log.d(TAG, "onCreateView: started");
 
         galleryImage = view.findViewById(R.id.galleryImageView);
-        gridView = view.findViewById(R.id.gridImageView);
+        gridView = view.findViewById(R.id.galleryGridView);
         mProgressBar = view.findViewById(R.id.galleryProgressBar);
         directorySpinner = view.findViewById(R.id.gallerySpinnerDirectory);
+        directories = new ArrayList<>();
 
         mProgressBar.setVisibility(View.GONE);
         ImageView shareClose = view.findViewById(R.id.ivCloseShare);
@@ -53,6 +72,91 @@ public class GalleryFragment extends Fragment {
             }
         });
 
+        init();
+
         return view;
+    }
+
+    private void init() {
+
+        FilePaths filePaths = new FilePaths();
+        //check for other folder inside "/storage/emulated/0/pictures"
+        if (FileSearch.getDirectoryPaths(filePaths.PICTURES) != null) {
+            directories = FileSearch.getDirectoryPaths(filePaths.PICTURES);
+        }
+        directories.add(filePaths.CAMERA);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, directories);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        directorySpinner.setAdapter(adapter);
+
+        directorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemSelected: item selected");
+
+                //setup our image grid for the directory chosen
+                setupGridView(directories.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    private void setupGridView(String selectedDirectory) {
+        Log.d(TAG, "setupGridView: directory chosen " + selectedDirectory);
+        final ArrayList<String> imgURLs = FileSearch.getFilePaths(selectedDirectory);
+
+        //set the grid column width
+        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+        gridView.setColumnWidth(imageWidth);
+
+        //use the grid adapter to adapter the images to gridView
+        GridImageAdapter adapter = new GridImageAdapter(getActivity(), R.layout.layout_grid_imageview, mAppend, imgURLs );
+        gridView.setAdapter(adapter);
+
+        //set the first image to be displayed when the activity frgment view is clicked or verified
+        setImage(imgURLs.get(0), galleryImage, mAppend);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected an image");
+                setImage(imgURLs.get(position), galleryImage, mAppend);
+            }
+        });
+    }
+
+    private void setImage(String imgURL, ImageView image, String append) {
+        Log.d(TAG, "setImage: setting image");
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
