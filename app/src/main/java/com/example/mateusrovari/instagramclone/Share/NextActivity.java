1dfa6c3ef839a4d95cd72cc33e13b3fe95aa7089
Mateus Rovari <mateus@liveonsolutions.com>
2018-08-20 17:48:36 -0300

    NextActivity Setup

--- app/src/main/java/com/example/mateusrovari/instagramclone/Share/NextActivity.java
+++ app/src/main/java/com/example/mateusrovari/instagramclone/Share/NextActivity.java
@@ -1,21 +1,128 @@
 package com.example.mateusrovari.instagramclone.Share;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mateusrovari.instagramclone.R;
+import com.example.mateusrovari.instagramclone.Utils.FirebaseMethods;
+import com.example.mateusrovari.instagramclone.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class NextActivity extends AppCompatActivity {
 
     private static final String TAG = "NextActivity";
+
+    //    firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListner;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
+
+    private String mAppend = "file:/";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        Log.d(TAG, "onCreate: got the chosen image" +  getIntent().getStringExtra(getString(R.string.selected_image)));
+        setupFirebaseAuth();
+
+        ImageView backArrow = findViewById(R.id.backArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        TextView share = findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to the final share screen");
+
+//                upload the image to firebase
+            }
+        });
+
+        setImage();
+    }
+
+    /**
+     * gets the image url from the incoming intent and displays the chosen image
+     */
+    private void setImage() {
+        Intent i = getIntent();
+        ImageView image = findViewById(R.id.imageShare);
+        UniversalImageLoader.setImage(i.getStringExtra(getString(R.string.selected_image)), image, null, mAppend);
+    }
+
+    /**
+     ---------------------------------- Firebase -------------------------------------
+     */
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebase: starting");
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    //user is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in" + user.getUid());
+                } else {
+                    //user is signed out
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListner != null) {
+            mAuth.removeAuthStateListener(mAuthListner);
+        }
     }
 }
