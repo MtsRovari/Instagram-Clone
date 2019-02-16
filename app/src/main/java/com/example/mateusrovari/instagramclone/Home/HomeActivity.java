86ff32c933d8ab233ba4c84ffdc4d6816ccd6414
Mateus Rovari <mateus@liveonsolutions.com>
2019-02-16 14:31:17 -0200

    Fragment to Activity (Home and Comments)

--- app/src/main/java/com/example/mateusrovari/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/example/mateusrovari/instagramclone/Home/HomeActivity.java
@@ -4,31 +4,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.example.mateusrovari.instagramclone.Login.LoginActivity;
 import com.example.mateusrovari.instagramclone.R;
 import com.example.mateusrovari.instagramclone.Utils.BottomNavigationViewHelper;
+import com.example.mateusrovari.instagramclone.Utils.MainFeedListAdapter;
 import com.example.mateusrovari.instagramclone.Utils.SectionsPagerAdapter;
 import com.example.mateusrovari.instagramclone.Utils.UniversalImageLoader;
-import com.example.mateusrovari.instagramclone.Utils.ViewCommentsFragment;
+import com.example.mateusrovari.instagramclone.models.Comment;
 import com.example.mateusrovari.instagramclone.models.Photo;
 import com.example.mateusrovari.instagramclone.models.UserAccountSettings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
     private static final int ACTIVITY_NUM = 0;
+
+    //vars
+    private ArrayList<Photo> mPhotos;
+    private ArrayList<String> mFollowing;
+    private ListView mListView;
+    private MainFeedListAdapter mAdapter;
 
     private Context mContext = HomeActivity.this;
 
@@ -42,26 +61,133 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting..");
+        mListView = findViewById(R.id.listView);
+        mFollowing = new ArrayList<>();
+        mPhotos = new ArrayList<>();
 
         setupFirebase();
         initImageLoader();
         setupBottomNavigationView();
-        setupViewPager();
 
+        getFollowing();
+//        setupViewPager();
+
+    }
+
+    private void getFollowing() {
+        Log.d(TAG, "getFollowing: searching for following");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference
+                .child(getString(R.string.dbname_following))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+                    Log.d(TAG, "onDataChange: found user: "
+                            + singleSnapshot.child(getString(R.string.field_user_id)).getValue());
+
+                    mFollowing.add(singleSnapshot.child(getString(R.string.field_user_id)).getValue().toString());
+                }
+                //get the photos
+                getPhotos();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getPhotos() {
+        Log.d(TAG, "getPhotos: gettint photos");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        for (int i = 0; i < mFollowing.size() ; i++) {
+            final int count = i;
+            Query query = reference
+                    .child(getString(R.string.dbname_user_photos))
+                    .child(mFollowing.get(i))
+                    .orderByChild(getString(R.string.field_user_id))
+                    .equalTo(mFollowing.get(i));
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
+
+                        Photo photo = new Photo();
+                        Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+
+                        photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
+                        photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                        photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
+                        photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                        photo.setData_created(objectMap.get(getString(R.string.field_data_created)).toString());
+                        photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+
+                        ArrayList<Comment> mComments = new ArrayList<Comment>();
+                        for (DataSnapshot dSnapshot : singleSnapshot
+                                .child(getString(R.string.field_comments)).getChildren()) {
+                            Comment comment = new Comment();
+                            comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
+                            comment.setComment(dSnapshot.getValue(Comment.class).getComment());
+                            comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
+                            mComments.add(comment);
+                        }
+
+                        photo.setComments(mComments);
+
+                        mPhotos.add(photo);
+
+                    }
+                    if (count >= mFollowing.size() -1) {
+                        //display our photos
+                        displayPhotos();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+    }
+
+    private void displayPhotos() {
+        if (mPhotos != null) {
+            Collections.sort(mPhotos, new Comparator<Photo>() {
+                @Override
+                public int compare(Photo o1, Photo o2) {
+                    return o2.getData_created().compareTo(o1.getData_created());
+                }
+            });
+
+            mAdapter = new MainFeedListAdapter(this, R.layout.layout_mainfeed_listitem, mPhotos);
+            mListView.setAdapter(mAdapter);
+        }
     }
     public void onCommentThreadSelected(Photo photo, UserAccountSettings settings) {
         Log.d(TAG, "onCommentThreadSelected: selected a comment thread");
 
-        ViewCommentsFragment fragment = new ViewCommentsFragment();
-        Bundle args = new Bundle();
-        args.putParcelable(getString(R.string.bundle_photo), photo);
-        args.putParcelable(getString(R.string.bundle_user_account_settings), settings);
-        fragment.setArguments(args);
+//        ViewCommentsActivity fragment = new ViewCommentsActivity();
+//        Bundle args = new Bundle();
+//        args.putParcelable(getString(R.string.bundle_photo), photo);
+//        args.putParcelable(getString(R.string.bundle_user_account_settings), settings);
+//        fragment.setArguments(args);
+//
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        transaction.replace(R.id.container, fragment);
+//        transaction.addToBackStack(getString(R.string.view_comments_fragment));
+//        transaction.commit();
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(getString(R.string.view_comments_fragment));
-        transaction.commit();
+        Intent i = new Intent(HomeActivity.this,  ViewCommentsActivity.class);
+        Log.e(TAG, "onCommentThreadSelected: photo " + photo);
+        Log.e(TAG, "onCommentThreadSelected: photo " + settings);
+        i.putExtra(getString(R.string.bundle_photo), photo);
+        i.putExtra(getString(R.string.bundle_user_account_settings), settings);
+        startActivity(i);
     }
 
     private void initImageLoader(){
@@ -72,18 +198,18 @@ public class HomeActivity extends AppCompatActivity {
 //    Responsible for adding the 3 tabs: Camera, Home, Messages
     private void setupViewPager(){
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new CameraFragment());
+//        adapter.addFragment(new CameraFragment());
         adapter.addFragment(new HomeFragment());
-        adapter.addFragment(new MessagesFragment());
+//        adapter.addFragment(new MessagesFragment());
         ViewPager viewPager = findViewById(R.id.container);
         viewPager.setAdapter(adapter);
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_messages);
+//        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_action_name);
+//        tabLayout.getTabAt(2).setIcon(R.drawable.ic_messages);
     }
 
 //    BottomNavigationView setup
